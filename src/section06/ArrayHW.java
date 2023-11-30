95900e9c7550dd9bf3de803f536f89c6ab15b277
ljiwo <ljiwo@DESKTOP-STU61FV>
2023-11-30 17:26:31 +0900

    .

---
+++ src/section06/ArrayHW.java
@@ -0,0 +1,40 @@
+package section06;
+
+public class ArrayHW {
+	public static void main(String[] args) {
+
+		int[][] nums = new int [7][7];
+		nums[0][0] = 1; nums[0][1] = 2; nums[0][2] = 3; nums[0][3] = 4; nums[0][4] = 5; nums[0][5] = 6; nums[0][6] = 7;
+		nums[1][0] = 8; nums[1][1] = 9; nums[1][2] = 10; nums[1][3] = 11; nums[1][4] = 12; nums[1][5] = 13; nums[1][6] = 14;
+		nums[2][0] = 15; nums[2][1] = 16; nums[2][2] = 17; nums[2][3] = 18; nums[2][4] = 19; nums[2][5] = 20; nums[2][6] = 21;
+		nums[3][0] = 22; nums[3][1] = 23; nums[3][2] = 24; nums[3][3] = 25; nums[3][4] = 26; nums[3][5] = 27; nums[3][6] = 28;
+		nums[4][0] = 29; nums[4][1] = 30; nums[4][2] = 31; nums[4][3] = 32; nums[4][4] = 33; nums[4][5] = 34; nums[4][6] = 35;
+		nums[5][0] = 36; nums[5][1] = 37; nums[5][2] = 38; nums[5][3] = 39; nums[5][4] = 40; nums[5][5] = 41; nums[5][6] = 42;
+		nums[6][0] = 43; nums[6][1] = 44; nums[6][2] = 45; nums[6][3] = 46; nums[6][4] = 47; nums[6][5] = 48; nums[6][6] = 49;
+
+//		int num = 0;
+//		for ( int i = 0; i < 7; i++ ) {
+//			for ( int j = 0; j < 7; j++ ) {
+//				num += nums[i];
+//			} 
+		}
+//		for (int i = 0; i < nums.length; i++) {
+//			for (int j = 0; j < nums.length; j++) {
+//				System.out.print(nums[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println("=================");
+//		System.out.println(nums[0][0]);
+//		int number = nums.length;
+//		int [] num1 = new int[number];
+//		if (number == 2)
+//			System.out.println(nums[i][j]);
+//		
+//		int 
+//		if ( nums[i] + nums[j] == 0)
+//			System.out.println(nums[i][j]);
+			
+		}
+
